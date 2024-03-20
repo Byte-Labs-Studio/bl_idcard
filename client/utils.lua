473f2a482e381b2776dcda5c07f93a5a3aa88822
Xirvin <xander228irvin@gmail.com>
2024-03-20 15:11:31 +1100

    Revert "fix(client/closestPed): switch to ox getClosestPlayer"
    This reverts commit 7e39ceb9bb82cf8c074b617162302c444414041b.

--- client/utils.lua
+++ client/utils.lua
@@ -49,12 +49,51 @@ function Utils.GetMugShot()
     return Citizen.Await(promiseId)
 end
 
+local function rotationToDirection(rotation)
+	local adjustedRotation = 
+	{ 
+		x = (math.pi / 180) * rotation.x, 
+		y = (math.pi / 180) * rotation.y, 
+		z = (math.pi / 180) * rotation.z 
+	}
+	local direction = 
+	{
+		x = -math.sin(adjustedRotation.z) * math.abs(math.cos(adjustedRotation.x)), 
+		y = math.cos(adjustedRotation.z) * math.abs(math.cos(adjustedRotation.x)), 
+		z = math.sin(adjustedRotation.x)
+	}
+	return direction
+end
+
+local function rayCastGamePlayCamera(distance)
+	local cameraRotation = GetGameplayCamRot()
+	local cameraCoord = GetGameplayCamCoord()
+	local direction = rotationToDirection(cameraRotation)
+	local destination = 
+	{ 
+		x = cameraCoord.x + direction.x * distance, 
+		y = cameraCoord.y + direction.y * distance, 
+		z = cameraCoord.z + direction.z * distance 
+	}
+	local a, b, c, d, e = GetShapeTestResult(StartShapeTestRay(cameraCoord.x, cameraCoord.y, cameraCoord.z, destination.x, destination.y, destination.z, -1, -1, 1))
+	return b, c, e
+end
+
 function Utils.GetPlayerLookingAt()
     local config = require "shared.config"
-    local playerId, entity = lib.getClosestPlayer(GetEntityCoords(cache.ped), config.range)
+    local range = config.range
 
-	if not entity or not IsPedAPlayer(entity) then return end
-	return GetPlayerServerId(playerId)
+    local hit, _, entity = rayCastGamePlayCamera(range)
+
+		if hit and IsEntityAPed(entity) then
+            local isPlayer = IsPedAPlayer(entity)
+            if isPlayer then
+                local serverId = GetPlayerServerId(NetworkGetPlayerIndexFromPed(entity))
+                return serverId
+            end
+
+		end
+
 end
 
 return Utils
