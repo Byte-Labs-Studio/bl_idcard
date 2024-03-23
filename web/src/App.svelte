7fe556ba39ba733f74f9897a9ca21f7f5897b602
Xirvin <xander228irvin@gmail.com>
2024-03-23 22:04:27 +1100

    fix(debug UI): Remove Debug from build UI

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -24,11 +24,14 @@
     </main>
 {/if}
 
-{#if $IS_BROWSER}
-    {#await import('./providers/Debug.svelte') then { default: Debug }}
-        <Debug />
-    {/await}
+{#if import.meta.env.DEV}
+    {#if $IS_BROWSER}
+        {#await import('./providers/Debug.svelte') then { default: Debug }}
+            <Debug />
+        {/await}
+    {/if}
 {/if}
+
 
 <style>
     main {
--- web/src/providers/Debug.svelte
+++ web/src/providers/Debug.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
+    import DebugImage from '@utils/debug/DebugImage.svelte';
     import { InitialiseDebugSenders } from '@utils/debug/init';
     import { InitialiseDebugReceivers } from '@utils/debug/receivers';
     import SendDebuggers from '@utils/debug/senders';
-    import debug_image from '@assets/debug_image.png';
     import { onMount } from 'svelte';
 
     onMount(() => {
@@ -107,8 +107,9 @@
         </ol>
     {/if}
 
-    <!-- <div
-        style="background-image: url({debug_image});"
-        class="absolute w-screen bg-fixed bg-no-repeat bg-center h-screen top-0 left-0 dev-image"
-    /> -->
+    <div
+        class="absolute w-screen bg-cover bg-no-repeat bg-center h-screen top-0 left-0 dev-image grid place-items-center"
+    >
+        <DebugImage />
+    </div>
 </div>
