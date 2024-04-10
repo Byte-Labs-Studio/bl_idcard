7d9866e475f04f16f94a8a882e3e28f0634116e5
Xirvin <103050557+ImXirvin@users.noreply.github.com>
2024-04-11 09:02:53 +1000

    Fix metadata check in item registration

--- server/init.lua
+++ server/init.lua
@@ -77,7 +77,7 @@ CreateThread(function()
     local items = config.items
     for k, v in pairs(items) do
         core.RegisterUsableItem(k, function(source, _, metadata)
-            if not metadata then
+            if not metadata or not metadata.idType then
                 local player = core.GetPlayer(source)
                 player.removeItem(k, 1)
                 metadata = createLicense(source, k, true)
